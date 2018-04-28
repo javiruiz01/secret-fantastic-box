ddaff9b723a46eb4515c6a1d785d8fbd74166b5a
Javier Ruiz Calle <jarc0212@gmail.com>
2018-04-28 19:05:37 +0200

    Complete the code to meet the assignment's specs

--- main.pl
+++ main.pl
@@ -8,34 +8,37 @@ esCuadradoFantasticoSecreto(Matriz, Secreto) :-
 
 recorrerMatriz([X | T], Secreto) :-
     reverse(T, [Y | _]),
-    recorrerListas(X, Y).
+    recorrerListas(X, Y, Secreto).
 
 recorrerListas([X | T], [Y | U], Secreto) :-
     reverse(T, [Xs | _]),
     reverse(U, [Ys | _]),
     comprobarSecreto(0, [X, Y, Xs, Ys], [X, Y, Xs, Ys], Secreto).
 
-comprobarSecreto(Y, [], Original, Secreto):-
-    sumar(Y, Original, [], Secreto).
 comprobarSecreto(0, [X | T], Original, Secreto) :-
+        comprobarSecreto(X, T, Original, Secreto).
+comprobarSecreto(Maximo, [X | T], Original, Secreto) :-
+    less_or_equal(Maximo, X),
     comprobarSecreto(X, T, Original, Secreto).
-comprobarSecreto(Y, [X | T], Original, Secreto) :-
-    less_or_equal(Y, X),
-    comprobarSecreto(X, T, Original, Secreto).
-comprobarSecreto(Y, [X | T], Original, Secreto) :-
-    comprobarSecreto(Y, T, Original, Secreto).
+comprobarSecreto(Maximo, [X | T], Original, Secreto) :-
+    % Comprobamos que es mayor que, por lo tanto se usa less_or_equal con los args al revés
+    less_or_equal(X, Maximo),
+    comprobarSecreto(Maximo, T, Original, Secreto).
+comprobarSecreto(Maximo, [], Original, Secreto):-
+    sumar(Maximo, Original, [], Secreto).
 
 sumar(Maximo, [X | T], Numeros, Secreto) :-
     Maximo = X,
-    sumar(Maximo, T, T, Secreto).
+    sumar(Maximo, T, Numeros, Secreto).
 sumar(Maximo, [X | T], Numeros, Secreto) :-
     % condicion para que no entre en caso de que sean iguales y entre por backtracing
-    sumar(Maximo, T, [X | T], Secreto).
+    Maximo \= X,
+    append([X], Numeros, Z),
+    sumar(Maximo, T, Z, Secreto).
 sumar(Maximo, [], [X, Y], Secreto) :-
     peano_add(X, Y, Z),
     Maximo = Z,
     Secreto = Z.
-
 
 % Métodos auxiliares
 nat(0).
@@ -50,3 +53,11 @@ less_or_equal(s(X),s(Y)) :-
 peano_add(0, N, N).
 peano_add( s(N), M, s(Sum) ) :-
 	peano_add( N, M, Sum ).
+
+reverse([], []).
+reverse([X | Xs], Ys) :-
+    reverse(Xs, Zs),
+    append(Zs, [X], Ys).
+
+% Prueba
+% esCuadradoFantasticoSecreto([[s(s(0)),s(s(s(s(0)))),s(s(s(s(s(s(0))))))],[s(s(s(s(0)))),s(s(s(s(s(s(0)))))),s(s(0))],[s(s(s(s(s(s(0)))))),s(s(0)),s(s(s(s(0))))]],s(s(s(s(s(s(0))))))).
